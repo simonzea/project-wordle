44944bdd1fddf841f59c229c7813a8da685f2752
Simon Zea <szeag@unal.edu.co>
2024-04-07 23:46:06 -0500

    Exercise 5: winning and losing

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -17,22 +17,22 @@ console.info({ answer });
 
 function Game() {
   let [guessWords , setGuessWords] = React.useState([]);
-  let [done , setDone] = React.useState(0);
+  let [done , setDone] = React.useState(false);
 
   function addWord(word) {
     if(guessWords.length >= NUM_OF_GUESSES_ALLOWED)return;
     let checkedWord = checkGuess(word.guess,answer);
-    let complited = checkedWord.reduce((accumulator, currentValue) => currentValue.status === 'correct'? accumulator + 1: accumulator,
-    0)
+    let complited = !!(word.guess === answer);
     setDone(complited);
+
     let newGuessWords = [...guessWords, {checkedWord, id: Math.random()}];
     setGuessWords(newGuessWords);
   }
 
   return (
   <>
-    {done === 5 && <Banner attempts={guessWords.length} />}
-    {done !== 5 && guessWords.length >= NUM_OF_GUESSES_ALLOWED && <BannerBad answer={answer} />}
+    {done  && <Banner attempts={guessWords.length} />}
+    {!done  && guessWords.length >= NUM_OF_GUESSES_ALLOWED && <BannerBad answer={answer} />}
     <GuessResult guessWords={guessWords}/>
     <EnterGuess addWord={addWord}/>
   </>);
